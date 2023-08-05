366856e4bb72d40e36b9d15419e23e65eea057b0
matin <elliotmigh@gmail.com>
2023-08-05 07:26:11 +0330

    create function for welcome toast and call function in onCreate()

--- app/src/main/java/com/example/sanjabtodo/MainActivity.kt
+++ app/src/main/java/com/example/sanjabtodo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sanjabtodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.sanjabtodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //call welcomeToast function:
+        welcomeToast()
+    }
+
+    private fun welcomeToast() {
+        val toastText: String = "به اپلیکیشن مدریت کارها با سنجاب خوش آمدید"
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show()
     }
 }
